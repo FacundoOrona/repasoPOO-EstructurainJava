6fd6a8fb82dd794bf2cf901eba8803403be8b89c
FacundoOrona <fcndorona@gmail.com>
2025-05-06 13:42:07 -0300

    Punto 3.a
    3. Crear las siguientes subclases de Empleado que implementen Trabajable:
    A. Administrativo
    Tiene un atributo adicional sector.
    
    Implementa calcularSueldo() sumando 10% al salario base.
    
    Sobrescribe trabajar() para decir "Gestionando documentos del sector [sector]."

---
+++ POO/src/package/Administrativo.java
@@ -0,0 +1,29 @@
+/*3. Crear las siguientes subclases de Empleado que implementen Trabajable:
+A. Administrativo
+Tiene un atributo adicional sector.
+
+Implementa calcularSueldo() sumando 10% al salario base.
+
+Sobrescribe trabajar() para decir "Gestionando documentos del sector [sector]." */
+
+public class Administrativo extends Empleado implements ITrabajable{
+
+    private String sector;
+
+    public Administrativo(String n, int e, double sb, String s){
+        super(n, e, sb);
+        this.sector = s;
+    }
+
+    @Override
+    public double calcularSueldo() {
+        double salarioTotal = this.getSalarioBase() * 1.10;
+        return salarioTotal;
+    }
+
+    @Override
+    public void trabajar() {
+        System.out.println("Gestionando documentos del sector " + sector);
+    }
+
+}
