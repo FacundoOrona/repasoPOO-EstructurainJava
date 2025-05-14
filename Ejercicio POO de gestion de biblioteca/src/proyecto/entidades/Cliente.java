33135a61cd2acf8d014b9dbdba6838ccef96cc3b
FacundoOrona <fcndorona@gmail.com>
2025-05-14 16:31:49 -0300

    impl Clientes

--- Ejercicio POO de gestion de biblioteca/src/proyecto/entidades/Cliente.java
+++ Ejercicio POO de gestion de biblioteca/src/proyecto/entidades/Cliente.java
@@ -1,5 +1,17 @@
 package proyecto.entidades;
 
+import java.util.List;
+import proyecto.entidades.Prestamo;
+
 public class Cliente extends Usuario{
     
+    private List<Prestamo> prestamosActivos; 
+
+    public Cliente(){};
+
+    public Cliente(String nombre, String dni, String email, List<Prestamo> prestamosActivos){
+        super(nombre, dni, email);
+        this.prestamosActivos = prestamosActivos;
+    }
+
 }
