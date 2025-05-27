8daa242edeffcf5e307427b8f5fd9261ae06e8a4
FacundoOrona <fcndorona@gmail.com>
2025-05-27 18:30:16 -0300

    constructor sin array y leer clientes archivos

--- Ejercicio POO de gestion de biblioteca/src/proyecto/entidades/Cliente.java
+++ Ejercicio POO de gestion de biblioteca/src/proyecto/entidades/Cliente.java
@@ -8,12 +8,18 @@ public class Cliente extends Usuario {
     private List<Prestamo> prestamosActivos;
 
     public Cliente() {
+        super(); 
+        prestamosActivos = new ArrayList<>();
+    }
+
+    public Cliente(String nombre, String dni, String email) {
+        super(nombre, dni, email);
         prestamosActivos = new ArrayList<>();
     }
 
     public Cliente(String nombre, String dni, String email, List<Prestamo> prestamosActivos) {
         super(nombre, dni, email);
-        prestamosActivos = new ArrayList<>();
+        this.prestamosActivos = prestamosActivos;
     }
 
     public void setPrestamosActivos(List<Prestamo> prestamosActivos) {
@@ -24,6 +30,13 @@ public class Cliente extends Usuario {
         return prestamosActivos;
     }
 
-    
-
+    public static Cliente fromString(String linea) {
+        String[] partes = linea.split(",");
+        if (partes.length >= 3) {
+            return new Cliente(partes[0], partes[1], partes[2]);
+        } else {
+            return new Cliente();
+        }
+    }
 }
+
